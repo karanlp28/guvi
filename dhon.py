bda7a436ec525eda0e91ba5ee4f59031f73aa6ec
karanlp28 <46442151+karanlp28@users.noreply.github.com>
2019-02-15 15:52:40 +0530

    Update dhon.py

--- dhon.py
+++ dhon.py
@@ -2,7 +2,7 @@ a=input()
 flagg=0
 msd=["d","h","o","n","i"]
 for x in range(len(a)):
-  if(x not in ms or ms.count(a[x])!=a.count(a[x])):
+  if(x not in msd or msd.count(a[x])!=a.count(a[x])):
     flagg=1
   else:
     flag=0
